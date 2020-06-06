321c20be801c314c6ff4f0c43bcc165989d4c9cf
hoo29 <huwmcnamara@msn.com>
2020-06-06 10:06:39 +0100

    okay then

--- index.js
+++ index.js
@@ -1,62 +1,58 @@
 const ids = ['a', 'b', 'c'];
-const spamLimit = 1000; // ms
-let fetching = false;
-
+const spamLimit = 50; // ms
 let cache = {};
 let timeOfLastRequest = 0; // epoch
 
-function getRandInRange(min, max) {
-    return Math.floor(Math.random() * max) + min;
-}
+let mutex = Promise.resolve();
 
-function clearCache() {
+const getRandInRange = (min, max) => Math.floor(Math.random() * max) + min;
+
+const clearCache = () => {
     console.log('clearing cache');
     cache = {};
-}
+};
 
-async function doAThing(id) {
-    if (typeof cache[id] === 'undefined') {
-        console.log('need to fetch', id);
-        const hadToWait = fetching;
-        while (fetching) {
-            console.log('somebody else is fetching, waiting', id);
-            await new Promise(resolve => setTimeout(resolve, spamLimit));
-        }
-        if (hadToWait) {
-            console.log('had to wait', id);
-            await new Promise(resolve => setTimeout(resolve, spamLimit));
-        }
-        const value = await getDataFromRemote(id);
-        cache[id] = value;
-    } else {
-        console.log('present', id);
-    }
-}
+const cacheTimer = setInterval(clearCache, 2000);
 
-async function getDataFromRemote(id) {
+const doAThing = async (id) => {
+    mutex = mutex.then(async () => {
+        if (typeof cache[id] === 'undefined') {
+            console.log('need to fetch', id);
+            const now = Date.now();
+            if (timeOfLastRequest + spamLimit > now) {
+                console.log('need to wait', id);
+                await new Promise((resolve) =>
+                    setTimeout(resolve, timeOfLastRequest + spamLimit - now)
+                );
+            }
+            const value = await getDataFromRemote(id);
+            cache[id] = value;
+        } else {
+            console.log('present', id);
+        }
+    });
+};
+
+const getDataFromRemote = async (id) => {
     const now = Date.now();
     if (timeOfLastRequest + spamLimit > now) {
         throw new Error(`we are spamming the remote server ${id}`);
     } else {
         console.log('about to fetch', id);
         timeOfLastRequest = now;
-        fetching = true;
-        await new Promise(resolve =>
+        await new Promise((resolve) =>
             setTimeout(resolve, getRandInRange(200, 800))
         );
-        fetching = false;
         console.log('fetched', id);
         return 'some value';
     }
-}
+};
 
-const errHandler = error => {
+const errHandler = (error) => {
     clearInterval(cacheTimer);
     clearInterval(workTimer);
     console.error(error.message);
 };
-
-const cacheTimer = setInterval(clearCache, 2000);
 
 const workTimer = setInterval(async () => {
     try {
